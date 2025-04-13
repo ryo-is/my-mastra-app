a2f103ed208adc33b1496d080157928e70ad147a
ryo-is <ryosuke.izumi62@gmail.com>
2025-04-13 20:56:29 +0900

    first commit

---
+++ src/mastra/index.ts
@@ -0,0 +1,18 @@
+
+import { Mastra } from '@mastra/core/mastra';
+import { createLogger } from '@mastra/core/logger';
+import { weatherWorkflow } from './workflows';
+import { weatherAgent } from './agents';
+import { researchNetwork } from './networks';
+
+export const mastra = new Mastra({
+  workflows: { weatherWorkflow },
+  networks: {
+    researchNetwork,
+  },
+  agents: { weatherAgent },
+  logger: createLogger({
+    name: 'Mastra',
+    level: 'info',
+  }),
+});
